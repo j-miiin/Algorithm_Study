658124cf122e5d9af761a34a4a4c7546336b867e
jungmin <kimjm0825@gmail.com>
2022-11-08 19:20:29 +0900

    .

--- BAEKJOON/Graph Traversal/abcde_13023.java
+++ BAEKJOON/Graph Traversal/abcde_13023.java
@@ -1,7 +1,16 @@
 package baekjoon.graph_traversal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class abcde_13023 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
     }
 }
