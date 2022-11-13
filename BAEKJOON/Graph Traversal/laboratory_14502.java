66c2642073316b8459645332ced66b37fc53cf04
jungmin <kimjm0825@gmail.com>
2022-11-13 23:26:08 +0900

    백준 - 14502 연구소

--- BAEKJOON/Graph Traversal/laboratory_14502.java
+++ BAEKJOON/Graph Traversal/laboratory_14502.java
@@ -1,10 +1,12 @@
 package baekjoon.graph_traversal;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class laboratory_14502 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
     }
 }
