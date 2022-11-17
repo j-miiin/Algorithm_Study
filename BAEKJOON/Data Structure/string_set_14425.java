94a40832ae36bc09c0b9f96573c6d439f198f143
jungmin <kimjm0825@gmail.com>
2022-11-18 00:27:52 +0900

    백준 - 14425 문자열 집합 풀이

--- BAEKJOON/Data Structure/string_set_14425.java
+++ BAEKJOON/Data Structure/string_set_14425.java
@@ -4,8 +4,27 @@
 
 package baekjoon.data_structure;
 
-public class string_set_14425 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
+public class string_set_14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        ArrayList<String> strList = new ArrayList<>();
+        for (int i = 0; i < N; i++) strList.add(br.readLine());
+
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (strList.contains(str)) count++;
+        }
+        System.out.println(count);
     }
 }
