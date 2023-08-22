cdbe42d3687f343dcc7d7dd8cebead3fd9fec226
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-22 19:23:56 +0900

    프로그래머스 - 몫 구하기 풀이

---
+++ PROGRAMMERS/GetQuotient.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithm_Study.Programmers
+{
+    internal class GetQuotient
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.solution(7, 2));
+        }
+
+        static class Solution
+        {
+            public static int solution(int num1, int num2)
+            {
+                return (int)(num1 / num2);
+            }
+        }
+    }
+}
