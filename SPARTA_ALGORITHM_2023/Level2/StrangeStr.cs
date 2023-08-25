1831b8697ee45583cabdabefd707b88098c92dd8
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-25 17:59:23 +0900

    스파르타 코딩클럽 알고리즘 카타 - Lv2 31번 풀이

---
+++ SPARTA_ALGORITHM_2023/Level2/StrangeStr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level2
+{
+    internal class StrangeStr
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.solution("AA aa ZZ zz"));
+        }
+
+        public class Solution
+        {
+            public static string solution(string s)
+            {
+                string answer = "";
+                string[] strArr = s.Split(' ');
+                StringBuilder sb = new StringBuilder();
+                foreach (string str in strArr)
+                {
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (i % 2 == 0) sb.Append((str[i] + "").ToUpper());
+                         else sb.Append((str[i] + "").ToLower());
+                    }
+                    sb.Append(" ");
+                }
+                answer = sb.ToString().TrimEnd(' ');
+                return answer;
+            }
+        }
+    }
+}
