7f971ece522fe96276e96a038de59833296f9914
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-30 10:04:19 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv2 45~46 풀이

---
+++ SPARTA_ALGORITHM_2023/Level2/45_CardDeck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level2
+{
+    internal class CardDeck
+    {
+        public class Solution
+        {
+            public string solution(string[] cards1, string[] cards2, string[] goal)
+            {
+                bool isPossible = true;
+                int idx1 = 0, idx2 = 0;
+                for (int i = 0; i < goal.Length; i++)
+                {
+                    if (idx1 < cards1.Length && goal[i] == cards1[idx1]) idx1++;
+                    else if (idx2 < cards2.Length && goal[i] == cards2[idx2]) idx2++;
+                    else
+                    {
+                        isPossible = false;
+                        break;
+                    }
+                }
+
+                string answer = isPossible ? "Yes" : "No";
+                return answer;
+            }
+        }
+    }
+}
---
+++ SPARTA_ALGORITHM_2023/Level2/46_FruitSeller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level2
+{
+    internal class FruitSeller
+    {
+        public class Solution
+        {
+            public int solution(int k, int m, int[] score)
+            {
+                int answer = 0;
+
+                Array.Sort(score);
+                Array.Reverse(score);
+                for (int i = m - 1; i < score.Length; i += m)
+                {
+                    answer += (score[i] * m);
+                }
+
+                return answer;
+            }
+        }
+    }
+}
