c72d77d0828e2a187988ab8c9782d47e2329889e
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-31 10:05:02 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv2 47~48 풀이

---
+++ SPARTA_ALGORITHM_2023/Level2/47_MockTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level2
+{
+    internal class MockTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Solution.solution(new int[] { 1, 3, 2, 4, 2 })));
+        }
+        public class Solution
+        {
+            public static int[] solution(int[] answers)
+            {
+                int[] person1Ans = new int[] { 1, 2, 3, 4, 5 };
+                int[] person2Ans = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
+                int[] person3Ans = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+                int[] countAns = new int[3];
+
+                for (int i = 0; i < answers.Length; i++)
+                {
+                    if (answers[i] == person1Ans[i % person1Ans.Length]) countAns[0]++;
+                    if (answers[i] == person2Ans[i % person2Ans.Length]) countAns[1]++;
+                    if (answers[i] == person3Ans[i % person3Ans.Length]) countAns[2]++;
+                }
+                
+                List<int> answerList = new List<int>();
+
+                for (int i = 0; i < countAns.Length; i++)
+                {
+                    if (countAns[i] == countAns.Max()) answerList.Add(i + 1);
+                }
+
+                int[] answer = answerList.ToArray();
+
+                return answer;
+            }
+        }
+    }
+}
---
+++ SPARTA_ALGORITHM_2023/Level2/48_MakePrimeNum.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level2
+{
+    internal class MakePrimeNum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.solution(new int[] { 1, 2, 7, 6, 4 }));
+        }
+
+        class Solution
+        {
+            static int[] numArr;
+            static bool[] visisted;
+            static int result;
+            public static int solution(int[] nums)
+            {
+                numArr = nums;
+                visisted = new bool[numArr.Length];
+                result = 0;
+
+                dfs(0, 0, 0);
+
+                int answer = result;
+                return answer;
+            }
+
+            static void dfs(int start, int depth, int sum)
+            {
+                if (depth == 3)
+                {
+                    if (isPrime(sum)) result++;
+                    return;
+                }
+
+                for (int i = start; i < numArr.Length; i++)
+                {
+                    if (!visisted[i])
+                    {
+                        visisted[i] = true;
+                        dfs(i + 1, depth + 1, sum + numArr[i]);
+                        visisted[i] = false;
+                    }
+                }
+            }
+
+            static bool isPrime(int num)
+            {
+                if (num == 0 || num == 1) return false;
+                if (num == 2) return true;
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if (num % i == 0) return false;
+                }
+                return true;
+            }
+        }
+    }
+}
