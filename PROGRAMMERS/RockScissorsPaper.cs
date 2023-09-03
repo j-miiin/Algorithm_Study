20ceb89780fa662cd63fb5d6f7fd6db0be9b180e
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-03 23:07:24 +0900

    프로그래머스 가위바위보 풀이

---
+++ PROGRAMMERS/RockScissorsPaper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Programmers
+{
+    internal class RockScissorsPaper
+    {
+        public class Solution
+        {
+            public string solution(string rsp)
+            {
+                string answer = "";
+                for (int i = 0; i < rsp.Length; i++)
+                {
+                    if (rsp[i] == '2') answer += "0";
+                    else if (rsp[i] == '0') answer += "5";
+                    else answer += "2";
+                }
+                return answer;
+            }
+        }
+    }
+}
