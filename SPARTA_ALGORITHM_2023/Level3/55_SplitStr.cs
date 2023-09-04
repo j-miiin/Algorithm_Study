7d4eb978d1a53fc61d270ea874765a8cc856f348
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-04 17:22:19 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 55 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/55_SplitStr.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class SplitStr
+    {
+        public class Solution
+        {
+            public int solution(string s)
+            {
+                List<string> resultStr = new List<string>();
+                string x = s[0] + "";
+                int same = 0, diff = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    string curStr = s[i] + "";
+                    if (x == curStr) same++;
+                    else diff++;
+
+                    if (same > 0 && diff > 0 && same == diff)
+                    {
+                        resultStr.Add(x);
+                        same = 0;
+                        diff = 0;
+                        if (i != s.Length - 1) x = s[i + 1] + "";
+                    }
+                }
+
+                if (same != diff) resultStr.Add(x);
+
+                int answer = resultStr.Count;
+                return answer;
+            }
+        }
+    }
+}
