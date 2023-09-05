1072c0f6f8a51ea58788ebda58b1808d494895ae
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-05 10:00:47 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 56~57 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/56_RoughKeyboard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class RoughKeyboard
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Solution.solution(new string[] { "ABACD", "BCEFD" }, new string[] { "ABCD", "AABB"})));
+        }
+
+        public class Solution
+        {
+            public static int[] solution(string[] keymap, string[] targets)
+            {
+                int[] answer = new int[targets.Length];
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    bool isPossible = true;
+                    int sum = 0;
+                    for (int j = 0; j < targets[i].Length; j++)
+                    {
+                        int minCnt = int.MaxValue;
+                        foreach (string key in keymap)
+                        {
+                            int cnt = key.IndexOf(targets[i][j]);
+                            if (minCnt > cnt && cnt >= 0) minCnt = cnt + 1;
+                        }
+
+                        if (minCnt == int.MaxValue)
+                        {
+                            isPossible = false;
+                            break;
+                        }
+                        else sum += minCnt;
+                    }
+                    if (isPossible) answer[i] = sum;
+                    else
+                    {
+                        answer[i] = -1;
+                        isPossible = true;
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
+}
---
+++ SPARTA_ALGORITHM_2023/Level3/57_CodeOfUs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class CodeOfUs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.solution("zzzzz", "a", 1));
+        }
+
+        public class Solution
+        {
+            public static string solution(string s, string skip, int index)
+            {
+                string answer = "";
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char curStr = s[i];
+
+                    for (int j = 0; j < index; j++)
+                    {
+                        curStr = (char)(curStr + 1);
+                        if (curStr.CompareTo('z') > 0) curStr = (char)(curStr - 26);
+                        while (skip.Contains(curStr)) curStr = (char)(curStr + 1);
+                    }
+                    answer += curStr;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
