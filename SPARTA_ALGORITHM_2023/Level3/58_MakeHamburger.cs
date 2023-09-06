6a8c369782e4e4d788278df1b323653d357b4bc3
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-06 09:20:04 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 58 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/58_MakeHamburger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class MakeHamburger
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.solution(new int[] { 1, 3, 2, 3, 1 }));
+        }
+
+        public class Solution
+        {
+            static Stack<int> burgerStack;
+            public static int solution(int[] ingredient)
+            {
+                int answer = 0;
+                burgerStack = new Stack<int>();
+                for (int i = 0; i < ingredient.Length; i++)
+                {
+                    int cur = ingredient[i];
+                    if (cur == 1)
+                    {
+                        if (burgerStack.Count > 0 && burgerStack.Peek() == 3)
+                        {
+                            RemoveBurgerStack();
+                            answer++;
+                        } else
+                        {
+                            burgerStack.Push(cur);
+                        }
+                    } else 
+                    {
+                        if (burgerStack.Count > 0) {
+                            if (burgerStack.Peek() == cur - 1)
+                            {
+                                burgerStack.Push(cur);
+                            } else
+                            {
+                                burgerStack.Clear();
+                            }
+                        }
+                    } 
+                }
+                return answer;
+            }
+
+            static void RemoveBurgerStack()
+            {
+                int prev = 0;
+                do
+                {
+                    prev = burgerStack.Pop();
+                } while (prev != 1);
+            }
+        }
+    }
+}
