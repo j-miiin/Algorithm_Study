eefb2daac8bf9ab80ec4d3f75aeac4d7861b95fa
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-11 11:02:16 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 64 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/64_ReportResult.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class ReportResult
+    {
+        public class Solution
+        {
+            public int[] solution(string[] id_list, string[] report, int k)
+            {
+                Dictionary<string, HashSet<string>> reportDic = new Dictionary<string, HashSet<string>>();
+                foreach (string id in id_list)
+                {
+                    reportDic.Add(id, new HashSet<string>());
+                }
+
+                foreach (string r in report)
+                {
+                    string[] strArr = r.Split(' ');
+                    string reporter = strArr[0];
+                    string reported = strArr[1];
+
+                    reportDic.GetValueOrDefault(reported).Add(reporter);
+                }
+
+                Dictionary<string, int> mailCnt = new Dictionary<string, int>();
+                foreach (string id in id_list)
+                {
+                    mailCnt.Add(id, 0);
+                }
+                foreach (KeyValuePair<string, HashSet<string>> entry in reportDic) 
+                {
+                    HashSet<string> curReporters = reportDic.GetValueOrDefault(entry.Key);
+                    if (curReporters.Count >= k)
+                    {
+                        foreach (string name in curReporters)
+                        {
+                            mailCnt[name] = mailCnt.GetValueOrDefault(name) + 1;
+                        }
+                    }
+                }
+
+                int[] answer = new int[id_list.Length];
+                int idx = 0;
+                foreach (KeyValuePair<string, int> entry in mailCnt)
+                {
+                    answer[idx++] = entry.Value;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
