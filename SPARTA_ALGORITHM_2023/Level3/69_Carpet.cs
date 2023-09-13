24520b878352688448195591118a8590e0f88a74
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-13 09:22:38 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 69 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/69_Carpet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class Carpet
+    {
+        public class Solution
+        {
+            public int[] solution(int brown, int yellow)
+            {
+                int carpet = brown + yellow;
+                int resultX = 0, resultY = 0;
+                bool isBreak = false;
+
+                for (int i = 1; i <= carpet / 2; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (i * j > carpet) break;
+                        if (i * j == carpet && (i - 2)*(j - 2) == yellow)
+                        {
+                            resultX = i;
+                            resultY = j;
+                            isBreak = true;
+                            break;
+                        }
+                    }
+                    if (isBreak) break;
+                }
+
+                int[] answer = new int[] { resultX, resultY };
+                return answer;
+            }
+        }
+    }
+}
