c948b594144ef125a7f11dc01d3e4c416ec4c2ff
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-15 09:51:07 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 71~72 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/71_LCMs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class LCMs
+    {
+        public class Solution
+        {
+            public int solution(int[] arr)
+            {
+                Stack<int> stack = new Stack<int>();
+                foreach (int i in arr) stack.Push(i);
+
+                while (stack.Count > 1)
+                {
+                    int n1 = stack.Pop();
+                    int n2 = stack.Pop();
+
+                    int newLcm = lcm(n1, n2);
+                    stack.Push(newLcm);
+                }
+
+                int answer = stack.Pop();
+                return answer;
+            }
+
+            static int gcd(int n, int m)
+            {
+                if (m == 0) return n;
+                else return gcd(m, n % m);
+            }
+
+            static int lcm(int n, int m)
+            {
+                return (n * m) / gcd(n, m);
+            }
+        }
+    }
+}
---
+++ SPARTA_ALGORITHM_2023/Level3/72_LongJump.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class LongJump
+    {
+        public class Solution
+        {
+            public long solution(int n)
+            {
+                if (n == 1) return 1;
+                long[] dp = new long[n + 1];
+                dp[1] = 1;
+                dp[2] = 2;
+
+                for (int i = 3; i <= n; i++)
+                {
+                    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+                }
+
+                long answer = dp[n];
+                return answer;
+            }
+        }
+    }
+}
