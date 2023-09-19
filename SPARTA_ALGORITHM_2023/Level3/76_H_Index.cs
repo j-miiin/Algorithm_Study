068e5c2881e1bed80c443c38632c9cfe30772302
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-19 09:53:48 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 76~77 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/76_H_Index.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class H_Index
+    {
+        public class Solution
+        {
+            public int solution(int[] citations)
+            {
+                int answer = 0;
+
+                for (int i = 1; i <= citations.Length; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < citations.Length; j++)
+                    {
+                        if (i <= citations[j]) count++;
+                    }
+
+                    if (count >= i && count > answer) answer = count;
+                }
+                
+                return answer;
+            }
+        }
+    }
+}
---
+++ SPARTA_ALGORITHM_2023/Level3/77_SliceArr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class SliceArr
+    {
+        public class Solution
+        {
+            public int[] solution(int n, long left, long right)
+            {
+                int[] answer = new int[right - left + 1];
+                int idx = 0;
+                for (long i = left; i <= right; i++)
+                {
+                    answer[idx++] = Math.Max((int)(i / n), (int)(i % n)) + 1;
+                }
+                return answer;
+            }
+        }
+    }
+}
