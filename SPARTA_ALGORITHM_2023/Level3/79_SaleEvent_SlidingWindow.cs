bbf1fb9eabe537acaf0a7fe8c54426aa02a93850
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-21 09:44:55 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 79 슬라이딩 윈도우 풀이, 80 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/79_SaleEvent_SlidingWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class SaleEvent_SlidingWindow
+    {
+        static Dictionary<string, int> wantDic;
+        static Dictionary<string, int> curDic;
+        public int solution(string[] want, int[] number, string[] discount)
+        {
+            int answer = 0;
+
+            InitDic(want, number);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (!curDic.ContainsKey(discount[i])) curDic.Add(discount[i], 1);
+                else curDic[discount[i]]++;
+            }
+
+            if (IsPossible()) answer++;
+
+            for (int i = 0, j = 10; j < discount.Length; i++, j++)
+            {
+                curDic[discount[i]]--;
+                if (!curDic.ContainsKey(discount[j])) curDic.Add(discount[j], 1);
+                else curDic[discount[j]]++;
+
+                if (IsPossible()) answer++;
+            }
+
+            return answer;
+        }
+
+        static void InitDic(string[] want, int[] number)
+        {
+            wantDic = new Dictionary<string, int>();
+            curDic = new Dictionary<string, int>();
+
+            for (int i = 0; i < want.Length; i++)
+            {
+                wantDic.Add(want[i], number[i]);
+                curDic.Add(want[i], 0);
+            }
+        }
+
+        static bool IsPossible()
+        {
+            foreach (string key in wantDic.Keys)
+            {
+                if (wantDic[key] > curDic[key]) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SPARTA_ALGORITHM_2023/Level3/80_Clothes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class Clothes
+    {
+        public class Solution
+        {
+            public int solution(string[,] clothes)
+            {
+                Dictionary<string, List<string>> clothesDic = new Dictionary<string, List<string>>();
+                for (int i = 0; i < clothes.GetLength(0); i++)
+                {
+                    string key = clothes[i, 1];
+                    string value = clothes[i, 0];
+
+                    if (!clothesDic.ContainsKey(key))
+                    {
+                        clothesDic.Add(key, new List<string>());
+                    } 
+                    clothesDic[key].Add(value);
+                }
+
+                int comb = 1;
+                foreach (string key in clothesDic.Keys)
+                {
+                    comb *= clothesDic[key].Count + 1;
+                }
+
+                int answer = comb - 1;
+                return answer;
+            }
+        }
+    }
+}
