f2365a7c471f3f48f077db2e554aeada8bbe48ac
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-22 09:26:36 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 81 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/81_DevelopFunction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class DevelopFunction
+    {
+        public class Solution
+        {
+            public int[] solution(int[] progresses, int[] speeds)
+            {
+                List<int> result = new List<int>();
+                int max = 0;
+                int count = 0;
+                for (int i = 0; i < progresses.Length; i++)
+                {
+                    progresses[i] = GetRemainingDay(progresses[i], speeds[i]);
+                    if (i == 0) max = progresses[i];
+                    if (progresses[i] > max)
+                    {
+                        max = progresses[i];
+                        result.Add(count);
+                        count = 1;
+                    } else {
+                        count++;
+                    }
+                }
+                result.Add(count);
+
+                int[] answer = result.ToArray();
+                return answer;
+            }
+
+            static int GetRemainingDay(int progress, int speed)
+            {
+                int count = 0;
+                while (progress < 100)
+                {
+                    progress += speed;
+                    count++;
+                }
+                return count;
+            }
+        }
+    }
+}
