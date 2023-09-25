f48d0506b0314c937456ceab9b91c957c6b3147c
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-25 10:39:05 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv3 82 풀이

---
+++ SPARTA_ALGORITHM_2023/Level3/82_Process.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level3
+{
+    internal class Process
+    {
+        public class Solution
+        {
+            public int solution(int[] priorities, int location)
+            {
+                Queue<int[]> queue = new Queue<int[]>();
+
+                for (int i = 0; i < priorities.Length; i++)
+                {
+                    queue.Enqueue(new int[] {i, priorities[i]});
+                }
+
+                int answer = 0;
+
+                while (queue.Count > 0)
+                {
+                    int maxPriority = GetMaxPriority(queue);
+
+                    int[] curProcess = queue.Dequeue();
+                    int processNum = curProcess[0];
+                    int processPriority = curProcess[1];
+
+                    if (maxPriority != processPriority) queue.Enqueue(curProcess);
+                    else
+                    {
+                        answer++;
+                        if (processNum == location) break;
+                    }
+                }
+
+                return answer;
+            }
+
+            static int GetMaxPriority(Queue<int[]> queue)
+            {
+                int max = 0;
+                foreach (int[] process in queue)
+                {
+                    if (process[1] > max) max = process[1];
+                }
+                return max;
+            }
+        }
+    }
+}
