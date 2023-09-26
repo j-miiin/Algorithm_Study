c0242f6472ba20fc9ac13f3ffbbb0a536ef0c275
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-26 09:12:32 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 83 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/83_Fatigue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class Fatigue
+    {
+        public class Solution
+        {
+            static bool[] visited;
+            static int result = 0;
+            public int solution(int k, int[,] dungeons)
+            {
+                visited = new bool[dungeons.GetLength(0)];
+
+                dfs(k, 0, dungeons);
+
+                int answer = result;
+                return answer;
+            }
+
+            static void dfs(int curFatigue, int count, int[,] dungeons)
+            {
+                result = Math.Max(result, count);
+
+                for (int i = 0; i < dungeons.GetLength(0); i++)
+                {
+                    if (!visited[i])
+                    {
+                        int needFatigue = dungeons[i, 0];
+                        int useFatigue = dungeons[i, 1];
+
+                        if (curFatigue < needFatigue) continue;
+
+                        visited[i] = true;
+                        dfs(curFatigue - useFatigue, count + 1, dungeons);
+                        visited[i] = false;
+                    }
+                }
+            } 
+        }
+    }
+}
