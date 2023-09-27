9891ce0d8f4d31f158b2a69f76f3254d7f6de546
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-27 10:00:16 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 84 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/84_TargetNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class TargetNumber
+    {
+        public class Solution
+        {
+            public int solution(int[] numbers, int target)
+            {
+                int answer = 0;
+
+                for (int i = 0; i < (1 << numbers.Length); i++)
+                {
+                    int sum = 0;
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        if ((i & (1 << j)) != 0) sum += numbers[j];
+                        else sum -= numbers[j];
+                    }
+
+                    if (sum == target) answer++;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
