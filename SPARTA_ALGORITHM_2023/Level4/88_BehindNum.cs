bc6fa56d7558b68757036a6992a2ba50ebff35dd
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-06 09:40:33 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 88 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/88_BehindNum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class BehindNum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", " ,Solution.solution(new int[] { 9, 1, 5, 3, 6, 2 })));
+        }
+        class Solution
+        {
+            public static int[] solution(int[] numbers)
+            {
+                Stack<int> stack = new Stack<int>();
+                Array.Reverse(numbers);
+
+                int[] answer = new int[numbers.Length];
+                for (int i = 0; i < answer.Length; i++)
+                {
+                    if (stack.Count == 0)
+                    {
+                        answer[i] = -1;
+                        stack.Push(numbers[i]);
+                    }
+                    else if (stack.Peek() <= numbers[i])
+                    {
+                        while (stack.Count > 0)
+                        {
+                            stack.Pop();
+                            if (stack.Count == 0)
+                            {
+                                answer[i] = -1;
+                                stack.Push(numbers[i]);
+                                break;
+                            }
+                            else if (stack.Peek() > numbers[i])
+                            {
+                                answer[i] = stack.Peek();
+                                stack.Push(numbers[i]);
+                                break;
+                            }
+                        }
+                    } else
+                    {
+                        answer[i] = stack.Peek();
+                        stack.Push(numbers[i]);
+                    }
+                }
+
+                Array.Reverse(answer);
+                return answer;
+            }
+        }
+    }
+}
