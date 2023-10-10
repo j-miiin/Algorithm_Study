ed6e7df0366fe80c61aded7ae6b75e6a78158752
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-10 09:38:50 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 89 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/89_RollCake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class RollCake
+    {
+        public class Solution
+        {
+            public int solution(int[] topping)
+            {
+                int answer = 0;
+
+                HashSet<int> firstTopping = new HashSet<int>();
+                Dictionary<int, int> secondTopping = new Dictionary<int, int>();
+
+                firstTopping.Add(topping[0]);
+                for (int i = 1; i < topping.Length; i++)
+                {
+                    if (secondTopping.ContainsKey(topping[i])) secondTopping[topping[i]]++;
+                    else secondTopping.Add(topping[i], 1);
+                }
+
+                if (firstTopping.Count == secondTopping.Count) answer++;
+
+                for (int i = 1; i < topping.Length - 1; i++)
+                {
+                    int curTopping = topping[i];
+                    firstTopping.Add(curTopping);
+                    if ((secondTopping[curTopping] - 1) == 0) secondTopping.Remove(curTopping);
+                    else secondTopping[curTopping]--;
+
+                    if (firstTopping.Count == secondTopping.Count) answer++;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
