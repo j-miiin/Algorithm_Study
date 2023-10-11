3c3d568eb574f71c6e9279a993cdcf181b9a8cc4
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-11 09:40:44 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 90 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/90_ConvertNum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class ConvertNum
+    {
+        public class Solution
+        {
+            static int result;
+            public int solution(int x, int y, int n)
+            {
+                result = -1;
+
+                bfs(x, y, n);
+
+                int answer = result;
+                return answer;
+            }
+
+            static void bfs(int x, int y, int n)
+            {
+                Queue<int[]> queue = new Queue<int[]>();
+                HashSet<int> set = new HashSet<int>();
+
+                queue.Enqueue(new int[] { x, 0 });
+
+                while (queue.Count > 0)
+                {
+                    int[] cur = queue.Dequeue();
+                    int curNum = cur[0];
+                    int curCnt = cur[1];
+
+                    if (curNum == y)
+                    {
+                        result = curCnt;
+                        return;
+                    }
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        int nextNum = 0;
+                        switch (i)
+                        {
+                            case 0: nextNum = curNum + n; break;
+                            case 1: nextNum = curNum * 2; break;
+                            case 2: nextNum = curNum* 3; break;
+                        }
+
+                        if (nextNum <= y && !set.Contains(nextNum))
+                        {
+                            queue.Enqueue(new int[] { nextNum, curCnt + 1 });
+                            set.Add(nextNum);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
