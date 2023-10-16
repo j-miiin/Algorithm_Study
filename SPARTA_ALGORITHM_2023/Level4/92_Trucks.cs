4f17d3b582d191a16c8ad54fc7608c17eadab150
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-16 09:50:52 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 92 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/92_Trucks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class Trucks
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.solution(10, 12, new int[] { 4, 4, 4, 2, 2, 1, 1, 1, 1 }));
+        }
+
+        public class Solution
+        {
+            public static int solution(int bridge_length, int weight, int[] truck_weights)
+            {
+                int answer = 0;
+
+                int idx = 0;
+                int sum = 0;
+                List<int> list = new List<int>();
+                for (int i = 0; i < bridge_length; i++) list.Add(0);
+
+                while (list.Count > 0)
+                {
+                    answer++;
+
+                    sum -= list[0];
+                    list.RemoveAt(0);
+
+                    if (idx < truck_weights.Length)
+                    {
+                        if (sum + truck_weights[idx] <= weight)
+                        {
+                            sum += truck_weights[idx];
+                            list.Add(truck_weights[idx]);
+                            idx++;
+                        } else
+                        {
+                            list.Add(0);
+                        }
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
+}
