12ff9f9af9c5844c800e8b897baac58c35c941b4
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-18 09:31:47 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 93 풀이

---
+++ SPARTA_ALGORITHM_2023/Level4/93_MaxNum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class MaxNum
+    {
+        public class Solution
+        {
+            public string solution(int[] numbers)
+            {
+                string[] stringNums = new string[numbers.Length];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    stringNums[i] = numbers[i].ToString();
+                }
+
+                Array.Sort(stringNums, (s1, s2) =>
+                {
+                    return (s2 + s1).CompareTo(s1 + s2);
+                });
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < stringNums.Length; i++) sb.Append(stringNums[i]);
+                string answer = sb.ToString();
+
+                if (answer[0] == '0') answer = "0";
+                return answer;
+            }
+        }
+    }
+}
