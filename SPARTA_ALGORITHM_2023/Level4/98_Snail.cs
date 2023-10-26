03b7d2f8f7b958a735b9a29d67fe690455cabbbb
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-26 09:17:37 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 98 풀이

--- SPARTA_ALGORITHM_2023/Level4/98_Snail.cs
+++ SPARTA_ALGORITHM_2023/Level4/98_Snail.cs
@@ -17,64 +17,35 @@ namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
         {
             public static int[] solution(int n)
             {
-                if (n == 1) return new int[]{ 1 };
-
-                int length = (n * n + n) / 2;
-
                 int[,] snail = new int[n, n];
 
                 int[] dx = { 1, 0, -1 };
                 int[] dy = { 0, 1, -1 };
 
+                int x = 0, y = 0;
                 int dir = 0;
-                int startX = 0, startY = 0;
-                int curX = startX, curY = startY;
-
-                int cnt = 0;
-                int curLength = n - 1;
                 int curNum = 1;
-                while (curNum <= length)
+                while (true)
                 {
-                    Console.WriteLine($"curX {curX}, curY {curY}");
-                    Console.WriteLine($"curNum {curNum}");
-                    snail[curX, curY] = curNum++;
+                    snail[x, y] = curNum++;
 
-                    cnt++;
-                    Console.WriteLine($"cnt {cnt}, curLength {curLength}");
+                    int nextX = x + dx[dir];
+                    int nextY = y + dy[dir];
 
-                    if (cnt % curLength == 0)
+                    if (nextX == n || nextY == n || snail[nextX, nextY] != 0)
                     {
-                        cnt = 0;
                         dir = (dir + 1) % 3;
-                        Console.WriteLine($"dir {dir}");
+                        nextX = x + dx[dir];
+                        nextY = y + dy[dir];
 
-                        if (dir % 3 == 0)
-                        {
-                            curLength -= 2;
-
-                            startX += 2;
-                            startY += 1;
-                            curX = startX;
-                            curY = startY;
-                        }
+                        if (nextX == n || nextY == n || snail[nextX, nextY] != 0) break;
                     }
 
-                    curX += dx[dir];
-                    curY += dy[dir];
+                    x = nextX;
+                    y = nextY;
                 }
 
-                Console.WriteLine();
-
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j <= i; j++)
-                    {
-                        Console.Write($"{snail[i, j]} ");
-                    }
-                    Console.WriteLine();
-                }
-
-                int[] answer = new int[length];
+                int[] answer = new int[curNum-1];
 
                 int idx = 0;
                 for (int i = 0; i < n; i++)
