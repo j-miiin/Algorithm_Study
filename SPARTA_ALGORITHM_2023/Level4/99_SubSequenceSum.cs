4f2a13b2c6a5f5bcaab52d18c3335ccb69ec82cf
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-27 10:34:51 +0900

    스파르타 코딩클럽 알고리즘 코드 카타 - Lv4 99 풀이 중

---
+++ SPARTA_ALGORITHM_2023/Level4/99_SubSequenceSum.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class SubSequenceSum
+    {
+        public class Solution
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(Solution.solution(new int[] { 1, 2, 3, 4, 5 }, 7));
+            }
+            public static int[] solution(int[] sequence, int k)
+            {
+                int[] prefixSum = new int[sequence.Length];
+                prefixSum[0] = sequence[0];
+                for (int i = 1; i < sequence.Length; i++)
+                {
+                    prefixSum[i] = prefixSum[i - 1] + sequence[i];
+                }
+
+                int start = -1, end = 0;
+                int minStart = 0, minEnd = prefixSum.Length;
+                int sum = 0;
+                while (start <= end && end <= prefixSum.Length)
+                {
+                    Console.WriteLine($"start {start}, end {end}, sum {sum}");
+                    if (start == -1) sum = prefixSum[end];
+                    else if (end == prefixSum.Length) sum = prefixSum[end - 1];
+                    else sum = prefixSum[end] - prefixSum[start];
+
+                    if (sum > k) start++;
+                    else if (sum < k) end++;
+                    else
+                    {
+                        int length = minEnd - minStart + 1;
+                        int curLength = end - start + 1;
+                        if (start == -1) curLength = end + 1;
+
+                        if (curLength < length)
+                        {
+                            minStart = start;
+                            minEnd = end;
+                            if (length == 1) break;
+                        }
+                    }
+                }
+
+                int[] answer = new int[] { minStart, minEnd};
+                return answer;
+            }
+        }
+    }
+}
