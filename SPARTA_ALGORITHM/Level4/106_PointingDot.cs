0a00430b614d11fc5278f40ca5dd011ac26ed17a
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-03-31 17:26:13 +0900

    코드 카타 106 - [프로그래머스] 점 찍기 풀이

---
+++ SPARTA_ALGORITHM/Level4/106_PointingDot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class PointingDot
+    {
+        public class Solution
+        {
+            public long solution(int k, int d)
+            {
+                long answer = 0;
+                for (int i = 0; i <= d; i += k)
+                {
+                    long sub = (long)d * (long)d - (long)i * (long)i;
+                    answer += (int)Math.Sqrt(sub) / k + 1;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
