b47af7eb0b239819f70b0bf24fb0de813642e517
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-04-01 14:29:09 +0900

    코드 카타 107 - [프로그래머스] 마법의 엘리베이터 풀이

---
+++ SPARTA_ALGORITHM/Level4/107_MagicElevator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class MagicElevator
+    {
+        public class Solution
+        {
+            public int solution(int storey)
+            {
+                int answer = 0;
+
+                int digit = 10;
+                while(storey >= 1)
+                {
+                    if (storey % digit == 5)
+                    {
+                        if ((storey / 10) % digit >= 5)
+                            storey += 5;
+                        else
+                            storey -= 5;
+                        answer += 5;
+                    }
+                    else if (storey % digit > 5)
+                    {
+                        int tmp = 10 - (storey % digit);
+                        answer += tmp;
+                        storey += tmp;
+                    } else
+                    {
+                        answer += (storey % digit);
+                        storey -= (storey % digit);
+                    }
+                    storey /= 10;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
