c3ee335104885a32f26740cdc018c6cf4cf614cf
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-04-07 17:40:43 +0900

    코드 카타 Lv.4 110 - [프로그래머스] 멀쩡한 사각형 풀이

---
+++ SPARTA_ALGORITHM/Level4/110_Square.cs
@@ -0,0 +1,18 @@
+﻿namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level4
+{
+    internal class Square
+    {
+        public class Solution
+        {
+            public long solution(int w, int h)
+            {
+                long answer = 0;
+                for (long i = 1; i < w; i++)
+                {
+                    answer += (long)(-((double)h / w * i) + h);
+                }
+                return answer * 2;
+            }
+        }
+    }
+}
