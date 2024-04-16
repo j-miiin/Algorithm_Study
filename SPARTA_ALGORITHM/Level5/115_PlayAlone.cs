bae078bd04373d2d277f79e3293a82e04299dce4
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-04-16 16:15:29 +0900

    코드 카타 Lv.5 115 - [프로그래머스] 혼자 놀기의 달인 풀이

---
+++ SPARTA_ALGORITHM/Level5/115_PlayAlone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level5
+{
+    internal class PlayAlone
+    {
+        public class Solution
+        {
+            static int num;
+            static bool[] isOpen;
+            public int solution(int[] cards)
+            {
+                int answer = 0;
+                num = cards.Length;
+                isOpen = new bool[num];
+
+                for (int i = 0; i < num; i++)
+                {
+                    int result1 = OpenBox(i, cards);
+                    if (result1 == num) break;
+                    for (int j = 0; j < num; j++)
+                    {
+                        if (isOpen[j]) continue;
+                        int result2 = OpenBox(j, cards);
+                        answer = Math.Max(answer, result1 * result2);
+                    }
+                    isOpen = new bool[num];
+                }
+
+                return answer;
+            }
+
+            static int OpenBox(int start, int[] box)
+            {
+                int count = 0;
+                int idx = start;
+                while (count <= num)
+                {
+                    isOpen[idx] = true;
+                    count++;
+                    idx = box[idx] - 1;
+                    if (isOpen[idx]) break;
+                }
+                return count;
+            }
+        }
+    }
+}
