3991103fbf0685aacb81cd7b30a469342eabd1a6
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-04-22 17:51:47 +0900

    코드 카타 Lv.5 118 - [프로그래머스] 디펜스 게임 풀이

---
+++ SPARTA_ALGORITHM/Level5/118_DefenseGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level5
+{
+    internal class DefenseGame
+    {
+        public class Solution
+        {
+            public int solution(int n, int k, int[] enemy)
+            {
+                int answer = 0;
+                SortedDictionary<int, int> skillRound = new SortedDictionary<int, int>();
+                for (int i = 0; i < k && i < enemy.Length; i++)
+                {
+                    int key = enemy[i];
+                    if (!skillRound.ContainsKey(key))
+                        skillRound.Add(key, 0);
+                    skillRound[key]++;
+                    answer++;
+                }
+
+                for (int i = k; i < enemy.Length; i++)
+                {
+                    int minEnemy = skillRound.First().Key;
+                    if (minEnemy < enemy[i])
+                    {
+                        if (minEnemy <= n)
+                        {
+                            skillRound[minEnemy]--;
+                            if (skillRound[minEnemy] == 0) skillRound.Remove(minEnemy);
+                            int key = enemy[i];
+                            if (!skillRound.ContainsKey(key)) skillRound.Add(key, 0);
+                            skillRound[key]++;
+                            n -= minEnemy;
+                            answer++;
+                        }
+                        else break;
+                    } else
+                    {
+                        if (enemy[i] <= n)
+                        {
+                            n -= enemy[i];
+                            answer++;
+                        }
+                        else break;
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
+}
