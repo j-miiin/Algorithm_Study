48b775865d588fd7ff7bdbbaa9ac6b35c90ed561
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-04-26 12:15:21 +0900

    코드 카타 Lv.5 120 - [프로그래머스] 두 원 사이의 정수 쌍 풀이

---
+++ SPARTA_ALGORITHM/Level5/120_IntegerPairs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level5
+{
+    internal class IntegerPairs
+    {
+        public class Solution
+        {
+            public long solution(int r1, int r2)
+            {
+                long answer = 0;
+                double x1, x2;
+                for (int i = 1; i <= r2; i++)
+                {
+                    x2 = Math.Sqrt((long)r2 * (long)r2 - (long)i * (long)i);
+                    if (i < r1)
+                    {
+                        x1 = Math.Sqrt((long)r1 * (long)r1 - (long)i * (long)i);
+                        answer += (long)Math.Floor(x2) - (long)Math.Ceiling(x1) + 1;
+                    }
+                    else
+                    {
+                        answer += (long)Math.Floor(x2) + 1;
+                    }
+                }
+
+                return answer * 4;
+            }
+        }
+    }
+}
