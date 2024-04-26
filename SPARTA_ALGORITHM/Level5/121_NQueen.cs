e617bd2317e3c36d73d1638682f32dcf24de1513
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-04-26 17:20:28 +0900

    코드 카타 Lv.5 121 - [프로그래머스] N-Queen 풀이

---
+++ SPARTA_ALGORITHM/Level5/121_NQueen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_CS_Algorithm_Study_2023.Algorithm_Code_Kata.Level5
+{
+    internal class NQueen
+    {
+        public class Solution
+        {
+            static int[] board;
+            static int answer = 0;
+
+            public int solution(int n)
+            {
+                board = new int[n];
+
+                Dfs(0, n);
+
+                return answer;
+            }
+
+            static void Dfs(int idx, int n)
+            {
+                if (idx >= n)
+                {
+                    answer++;
+                    return;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    board[idx] = i;
+
+                    if (IsPossiblePoint(idx))
+                        Dfs(idx + 1, n);
+                }
+            }
+
+            static bool IsPossiblePoint(int idx)
+            {
+                for (int i = 0; i < idx; i++)
+                {
+                    if (board[i] == board[idx]) return false;
+                    if (Math.Abs(i - idx) == Math.Abs(board[i] - board[idx])) return false;
+                }
+                return true;
+            }
+        }
+    }
+}
