a577ec581cabde18c8d05ce24e7a37ab7bc54511
jmin <kimjm0825@gmail.com>
2024-10-13 15:47:18 +0900

    백준 - 1874 스택 수열 풀이

---
+++ BAEKJOON/Silver2/1874_StackSequence.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class StackSequence
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> sequence = new List<int>(n);
+            for (int i = 0; i < n; i++)
+            {
+                sequence.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int idx = 0;
+            Stack<int> stack = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                stack.Push(i);
+                sb.Append("+").Append("\n");
+
+                while (stack.Count > 0 && stack.Peek() == sequence[idx])
+                {
+                    stack.Pop();
+                    sb.Append("-").Append("\n");
+                    idx++;
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                Console.Write("NO");
+            } else
+            {
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+}
