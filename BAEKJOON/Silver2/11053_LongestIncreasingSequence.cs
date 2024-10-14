7e5ee8dd8dab5827d89ee4f448f5a4cb25a79772
jmin <kimjm0825@gmail.com>
2024-10-14 17:13:28 +0900

    백준 - 11053 가장 긴 증가하는 부분 수열 풀이

---
+++ BAEKJOON/Silver2/11053_LongestIncreasingSequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class LongestIncreasingSequence
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] num = new int[N];
+
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < str.Length; i++)
+            {
+                num[i] = int.Parse(str[i]);
+            }
+
+            int[] count = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                count[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (num[j] < num[i] && count[i] < count[j] + 1)
+                    {
+                        count[i] = count[j] + 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(count.Max());
+        }
+    }
+}
