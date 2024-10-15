8ef2b674e8d9579262a44a9c9e3f3493fe34cb2d
jmin <kimjm0825@gmail.com>
2024-10-15 17:39:10 +0900

    백준 - 2293 동전 1 풀이

---
+++ BAEKJOON/Gold4/2293_Coin1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold4
+{
+    internal class Coin1
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(" ");
+            int n = int.Parse(str[0]);
+            int k = int.Parse(str[1]);
+
+            int[] value = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                value[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(value);
+
+            int[] dp = new int[k + 1];
+            dp[0] = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= k; j++)
+                {
+                    if (value[i] <= j)
+                    {
+                        dp[j] += dp[j - value[i]];
+                    }
+                }
+            }
+
+            Console.WriteLine(dp[k]);
+        }
+    }
+}
