30b6233ab502488ca69ca27021af12b5b2b3e70a
jmin <kimjm0825@gmail.com>
2024-10-16 15:51:12 +0900

    백준 - 1912 연속합 풀이

---
+++ BAEKJOON/Silver2/1912_ContinuousSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class ContinuousSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(str[i]);
+            }
+
+            int[] sum = new int[n];
+            sum[0] = array[0];
+            int answer = Math.Max(int.MinValue, sum[0]);
+
+            for (int i = 1; i < n; i++)
+            {
+                sum[i] = Math.Max(sum[i - 1] + array[i], array[i]);
+                answer = Math.Max(answer, sum[i]);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
