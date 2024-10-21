eb9d0ee920cf164d92c461a36f3241b7d3165549
jmin <kimjm0825@gmail.com>
2024-10-22 00:20:38 +0900

    백준 - 17298 우큰수 풀이

---
+++ BAEKJOON/Gold4/17298_RightBigNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold4
+{
+    internal class RightBigNumber
+    {
+        class Num
+        {
+            public int idx;
+            public int value;
+
+            public Num (int i, int v)
+            {
+                idx = i;
+                value = v;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] result = new int[N];
+            Stack<Num> stack = new Stack<Num>();
+
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                int value = int.Parse(str[i]);
+
+                while (stack.Count > 0)
+                {
+                    if (stack.Peek().value >= value)
+                    {
+                        break;
+                    }
+                    
+                    Num peek = stack.Pop();
+                    result[peek.idx] = value;
+
+                }
+
+                stack.Push(new Num(i, value));
+            }
+
+            while (stack.Count > 0)
+            {
+                Num peek = stack.Pop();
+                result[peek.idx] = -1;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append(result[i]).Append(" ");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
