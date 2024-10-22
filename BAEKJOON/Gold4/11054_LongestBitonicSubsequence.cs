8697c9f30a1f3f8f559e3675abe52ad4576c2094
jmin <kimjm0825@gmail.com>
2024-10-23 01:43:31 +0900

    백준 - 11054 가장 긴 바이토닉 부분 수열 풀이

---
+++ BAEKJOON/Gold4/11054_LongestBitonicSubsequence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold4
+{
+    internal class LongestBitonicSubsequence
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] sequence = new int[N];
+
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                sequence[i] = int.Parse(str[i]);
+            }
+
+            int[] increase = new int[N];
+            int[] reverse_increase = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                increase[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (sequence[j] < sequence[i] && increase[i] < increase[j] + 1)
+                    {
+                        increase[i] = increase[j] + 1;
+                    }
+                }
+            }
+            
+            for (int i = N - 1; i >= 0; i--)
+            {
+                reverse_increase[i] = 1;
+                for (int j = N - 1; j > i; j--)
+                {
+                    if (sequence[j] < sequence[i] 
+                        && reverse_increase[i] < reverse_increase[j] + 1)
+                    {
+                        reverse_increase[i] = reverse_increase[j] + 1;
+                    }
+                }
+            }
+
+            int answer = int.MinValue;
+            for (int i = 0; i < N; i++)
+            {
+                answer = Math.Max(answer, increase[i] + reverse_increase[i]);
+            }
+
+            Console.WriteLine(answer - 1);
+        }
+    }
+}
