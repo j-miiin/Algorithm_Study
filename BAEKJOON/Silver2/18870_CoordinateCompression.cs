e7c12134e251d9ea09e88367e66bf8c36755a517
jmin <kimjm0825@gmail.com>
2024-10-29 21:19:56 +0900

    백준 - 18870 좌표 압축 풀이

---
+++ BAEKJOON/Silver2/18870_CoordinateCompression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class CoordinateCompression
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[N];
+            HashSet<int> numSet = new HashSet<int>();
+            Dictionary<int, int> pressedValueDic = new Dictionary<int, int>();
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                nums[i] = int.Parse(str[i]);
+                numSet.Add(nums[i]);
+            }
+
+            List<int> setList = numSet.ToList();
+            setList.Sort((n1, n2) => { return n2 - n1; });
+
+            for (int i = 0; i < setList.Count; i++)
+            {
+                pressedValueDic.Add(setList[i], setList.Count - i - 1);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sb.Append(pressedValueDic[nums[i]]).Append(" ");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
