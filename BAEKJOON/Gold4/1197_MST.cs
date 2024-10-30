23e1b42e96a6b7c18b9c9faa5d72f4a4b0546ddc
jmin <kimjm0825@gmail.com>
2024-10-30 21:09:52 +0900

    백준 - 1197 최소 스패닝 트리 풀이

---
+++ BAEKJOON/Gold4/1197_MST.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold4
+{
+    internal class MST
+    {
+        class Edge
+        {
+            public int v1, v2;
+            public int cost;
+
+            public Edge(int v1, int v2, int cost)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+                this.cost = cost;
+            }
+        }
+
+        static int V, E;
+        static int[] parent;
+
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(" ");
+            V = int.Parse(str[0]);
+            E = int.Parse(str[1]);
+
+            parent = new int[V + 1];
+            for (int i = 1; i <= V; i++)
+            {
+                parent[i] = i;
+            }
+
+            List<Edge> edges = new List<Edge>();
+            for (int i = 0; i < E; i++)
+            {
+                str = Console.ReadLine().Split(" ");
+                int v1 = int.Parse(str[0]);
+                int v2 = int.Parse(str[1]);
+                int cost = int.Parse(str[2]);
+
+                edges.Add(new Edge(v1, v2, cost));
+            }
+
+            edges.Sort((e1, e2) => { return e1.cost - e2.cost; });
+
+            int answer = 0;
+            for (int i = 0; i <  edges.Count; i++)
+            {
+                Edge e = edges[i];
+                if (!IsSameParent(e.v1, e.v2))
+                {
+                    answer += e.cost;
+                    Union(e.v1, e.v2);
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+
+        static int Find(int x)
+        {
+            if (x == parent[x])
+            {
+                return x;
+            }
+
+            return parent[x] = Find(parent[x]);
+        }
+
+        static void Union(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            if (x != y)
+            {
+                parent[y] = x;
+            }
+        }
+
+        static bool IsSameParent(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            return (x == y);
+        }
+    }
+}
