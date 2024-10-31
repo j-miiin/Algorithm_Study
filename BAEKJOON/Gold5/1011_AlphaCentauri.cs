c3e22b1ab800fd8033c42e60a2d7dfa6ad392cb9
jmin <kimjm0825@gmail.com>
2024-10-31 22:13:25 +0900

    백준 - 1011 Fly me to the Alpha Centauri 풀이

---
+++ BAEKJOON/Gold5/1011_AlphaCentauri.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold5
+{
+    internal class AlphaCentauri
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            string[] str;
+            for (int i = 0; i < T; i++)
+            {
+                str = Console.ReadLine().Split(" ");
+
+                int length = int.Parse(str[1]) - int.Parse(str[0]);
+                int n = (int)Math.Sqrt(length);
+                
+                if (n * n == length)
+                {
+                    sb.Append(2 * n - 1).Append("\n");
+                } else if (n * n < length && length <= n * n + n)
+                {
+                    sb.Append(2 * n).Append("\n");
+                } else
+                {
+                    sb.Append(2 * n + 1).Append("\n");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
