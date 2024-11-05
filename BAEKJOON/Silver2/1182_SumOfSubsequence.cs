71518bc8f27fb2b287fd965777e07d715fd11e10
jmin <kimjm0825@gmail.com>
2024-11-05 17:40:59 +0900

    백준 - 1182 부분수열의 합 풀이

---
+++ BAEKJOON/Silver2/1182_SumOfSubsequence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class SumOfSubsequence
+    {
+        static int answer;
+
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(" ");
+            int N = int.Parse(str[0]);
+            int S = int.Parse(str[1]);
+
+            int[] nums = new int[N];
+            str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                nums[i] = int.Parse(str[i]);
+            }
+
+            FindSubsequence(0, 0, S, nums);
+
+            Console.WriteLine((S == 0) ? answer - 1 : answer);
+        }
+
+        static void FindSubsequence(int start, int cur, int target, int[] nums)
+        {
+            if (cur == target)
+            {
+                answer++;
+            }
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                FindSubsequence(i + 1, cur + nums[i], target, nums);
+            }
+        }
+    }
+}
