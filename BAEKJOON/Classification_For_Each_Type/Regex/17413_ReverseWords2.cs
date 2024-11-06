0796da68340db4eab9eb09c06f44c90e41d429dc
jmin <kimjm0825@gmail.com>
2024-11-06 22:47:55 +0900

    백준 - 17413 단어 뒤집기 2 스택, Regex 풀이

---
+++ BAEKJOON/Classification_For_Each_Type/Regex/17413_ReverseWords2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Regex
+{
+    internal class ReverseWords2
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            bool isTag = false;
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '<')
+                {
+                    isTag = true;
+
+                    while (stack.Count > 0)
+                    {
+                        sb.Append(stack.Pop());
+                    }
+
+                    sb.Append(S[i]);
+                    continue;
+                } else if (S[i] == '>')
+                {
+                    isTag = false;
+                    sb.Append(S[i]);
+                    continue;
+                } 
+                
+                if (isTag)
+                {
+                    sb.Append(S[i]);
+                    continue;
+                }
+
+                if (S[i] == ' ')
+                {
+                    while (stack.Count > 0)
+                    {
+                        sb.Append(stack.Pop());
+                    }
+                    sb.Append(" ");
+                } else
+                {
+                    stack.Push(S[i]);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ BAEKJOON/Classification_For_Each_Type/Regex/17413_ReverseWords2_Regex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon
+{
+    internal class ReverseWords2_Regex
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            Regex regex = new Regex(@"<[a-z\s]+>|[a-z0-9]+");
+
+            int idx = 0;
+            foreach (var match in regex.Matches(S))
+            {
+                string cur = match.ToString();
+                if (cur[0] == '<')
+                {
+                    sb.Append(cur);
+                }
+                else
+                {
+                    foreach (var c in cur.Reverse())
+                    {
+                        sb.Append(c);
+                    }
+                }
+
+                idx += cur.Length;
+                if (idx < S.Length && S[idx] == ' ')
+                {
+                    sb.Append(" ");
+                    idx++;
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
