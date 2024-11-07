825914d6bae92603a51eae5441c61793541ce0ce
jmin <kimjm0825@gmail.com>
2024-11-07 16:04:58 +0900

    백준 - 1013 Contact 풀이

---
+++ BAEKJOON/Classification_For_Each_Type/Regex/1013_Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.RegexSolutions
+{
+    internal class Contact
+    {
+        static void Main(string[] args)
+        {
+            const string YES = "YES";
+            const string NO = "NO";
+
+            Regex regex = new Regex(@"^(100+1+|01)+$");
+
+            int T = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < T; i++)
+            {
+                if (regex.IsMatch(Console.ReadLine()))
+                {
+                    sb.Append(YES).Append("\n");
+                } else
+                {
+                    sb.Append(NO).Append("\n");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());   
+        }
+    }
+}
