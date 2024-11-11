8a26d7276ff9d1274c782a42df91646bf808a325
jmin <kimjm0825@gmail.com>
2024-11-11 17:27:04 +0900

    백준 - 2493 탑 풀이

---
+++ BAEKJOON/Gold5/2493_Tower.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold5
+{
+    internal class Tower
+    {
+        class Node
+        {
+            public int idx;
+            public int height;
+
+            public Node (int idx, int height)
+            {
+                this.idx = idx;
+                this.height = height;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] tower = new int[N];
+
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                tower[i] = int.Parse(str[i]);
+            }
+
+            int[] answer = new int[N];
+            int idx = N - 1;
+            Stack<Node> stack = new Stack<Node>();
+            for (int i = N - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0)
+                {
+                    if (stack.Peek().height < tower[i])
+                    {
+                        Node node = stack.Pop();
+                        answer[node.idx] = i + 1;
+                    } else
+                    {
+                        break;
+                    }
+                }
+                stack.Push(new Node(i, tower[i]));
+            }
+
+            while (stack.Count > 0)
+            {
+                Node node = stack.Pop();
+                answer[node.idx] = 0;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < answer.Length; i++)
+            {
+                sb.Append(answer[i]).Append(" ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
