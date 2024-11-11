c078b0cdbf06e617764ba5453b2c1ef7a8d64d19
jmin <kimjm0825@gmail.com>
2024-11-11 17:01:35 +0900

    백준 - 10799 쇠막대기 풀이

---
+++ BAEKJOON/Silver2/10799_IronRod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class IronRod
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            int answer = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    if (s[i + 1] == ')')
+                    {
+                        answer += stack.Count;
+                        i++;
+                    } else
+                    {
+                        stack.Push(s[i]);
+                    }
+                } else
+                {
+                    stack.Pop();
+                    answer++;
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
