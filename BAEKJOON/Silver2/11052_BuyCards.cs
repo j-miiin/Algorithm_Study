189aa227ec47bd4b74f2e3b794bb6afc53789d42
jmin <kimjm0825@gmail.com>
2024-11-13 17:32:12 +0900

    백준 - 11052 카드 구매하기 풀이

---
+++ BAEKJOON/Silver2/11052_BuyCards.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Silver2
+{
+    internal class BuyCards
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] cards = new int[N + 1];
+            int[] dp = new int[N + 1];
+
+            string[] str = Console.ReadLine().Split(" ");
+            for (int i = 1; i <= N; i++)
+            {
+                cards[i] = int.Parse(str[i - 1]);
+                dp[i] = cards[i];
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    dp[i] = Math.Max(dp[i], dp[i - j] + cards[j]);
+                }
+            }
+
+            Console.WriteLine(dp[N]);
+        }
+    }
+}
