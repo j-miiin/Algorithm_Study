7fc3141502b37f888fbaea1bbc8e3e58e33051ca
jmin <kimjm0825@gmail.com>
2024-11-15 17:56:04 +0900

    백준 - 2565 전깃줄 풀이

---
+++ BAEKJOON/Gold5/2565_Wire.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sparta_CS_Algorithm_Study_2023.Baekjoon.Gold5
+{
+    internal class Wire
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            List<int[]> wireList = new List<int[]>();
+            for (int i = 0; i < N; i++)
+            {
+                string[] str = Console.ReadLine().Split(" ");
+                int numA = int.Parse(str[0]);
+                int numB = int.Parse(str[1]);
+
+                wireList.Add(new int[] { numA, numB });
+            }
+
+            wireList.Sort((w1, w2) =>
+            {
+                return w1[0] - w2[0];
+            });
+
+            int[] dp = new int[N];
+            Array.Fill(dp, 1);
+
+            int max = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (wireList[j][1] < wireList[i][1])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                        max = Math.Max(max, dp[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine(N - max);  
+        }
+    }
+}
