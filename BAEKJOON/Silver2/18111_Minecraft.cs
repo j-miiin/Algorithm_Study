615097860ad6561fa8cb723033143c08916c242a
j-miiin <62470991+j-miiin@users.noreply.github.com>
2025-04-22 23:23:16 +0900

    백준 - 18111, 21736 풀이

---
+++ BAEKJOON/Silver2/18111_Minecraft.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmStudy.Baekjoon.Silver2
+{
+    class Minecraft
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(" ");
+            int N = int.Parse(str[0]);
+            int M = int.Parse(str[1]);
+            int B = int.Parse(str[2]);
+
+            int[,] ground = new int[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                str = Console.ReadLine().Split(" ");
+                for (int j = 0; j < M; j++)
+                {
+                    ground[i, j] = int.Parse(str[j]);
+                }
+            }
+
+            int minTime = int.MaxValue;
+            int resultHeight = -1;
+
+            for (int i = 256; i >= 0; i--)
+            {
+                int inventory = B;
+                int time = 0;
+
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < M; k++)
+                    {
+                        int curHeight = ground[j, k];
+
+                        if (curHeight < i)
+                        {
+                            time += (i - curHeight);
+                            inventory -= (i - curHeight);
+                        }
+                        else if (curHeight > i)
+                        {
+                            time += (curHeight - i) * 2;
+                            inventory += (curHeight - i);
+                        }
+                    }
+                }
+
+                if (inventory >= 0 && time < minTime)
+                {
+                    minTime = time;
+                    resultHeight = i;
+                } else if (inventory >= 0 && time == minTime && i >= resultHeight)
+                {
+                    minTime = time;
+                    resultHeight = i;
+                }
+            }
+
+            Console.WriteLine($"{minTime} {resultHeight}");
+        }
+    }
+}
---
+++ BAEKJOON/Silver2/21736_NeedFriends.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmStudy.Baekjoon.Silver2
+{
+    class NeedFriends
+    {
+        static int[] dx = { -1, 1, 0, 0 };
+        static int[] dy = { 0, 0, -1, 1 };
+
+        static int N, M;
+        static char[,] campus;
+
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(" ");
+            N = int.Parse(str[0]);
+            M = int.Parse(str[1]);
+
+            campus = new char[N, M];
+
+            int startX = 0;
+            int startY = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                for (int j = 0; j < M; j++)
+                {
+                    campus[i, j] = s[j];
+
+                    if (s[j] == 'I')
+                    {
+                        startX = i;
+                        startY = j;
+                    }
+                }
+            }
+
+            int result = Move(startX, startY);
+            Console.WriteLine((result == 0) ? "TT" : result);
+        }
+
+        static int Move(int x, int y)
+        {
+            bool[,] visited = new bool[N, M];
+            Queue<int[]> queue = new Queue<int[]>();
+
+            visited[x, y] = true;
+            queue.Enqueue(new int[]{ x, y });
+
+            int result = 0;
+
+            while (queue.Count > 0)
+            {
+                int[] cur = queue.Dequeue();
+                int curX = cur[0];
+                int curY = cur[1];
+
+                for (int i = 0; i < dx.Length; i++)
+                {
+                    int nextX = curX + dx[i];
+                    int nextY = curY + dy[i];
+
+                    if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M
+                        || visited[nextX, nextY] || campus[nextX, nextY] == 'X')
+                        continue;
+
+                    if (campus[nextX, nextY] == 'P')
+                        result++;
+
+                    visited[nextX, nextY] = true;
+                    queue.Enqueue(new int[] { nextX, nextY });
+                }
+            }
+
+            return result;
+        }
+    }
+}
